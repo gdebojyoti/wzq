a91211c708f78ab3803458e3680ebfb4e329f098
Debojyoti Ghosh <gdebojyoti@users.noreply.github.com>
2024-12-18 17:50:03 +0530

    Tailwind CSS v4.0 beta added. (#1)
    * PostCSS added.
    
    * Tailwind 4 beta installed.

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
 
 const Page = () => {
-  return <h1>I got in.</h1>
+  return <h1 className='text-red-500 bg-blue-300 text-xl text-green-300 '>
+    I got in
+  </h1>
 }
 
 export default Page
