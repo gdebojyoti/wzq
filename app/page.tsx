46eadebacd0995f1a0b9e458c4d07333f049ba17
Debojyoti Ghosh <gdebojyoti@users.noreply.github.com>
2024-12-18 17:34:39 +0530

    PostCSS added.

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
 
 const Page = () => {
-  return <h1>I got in.</h1>
+  return <h1 className='heading'>
+    <span>I got in</span>
+  </h1>
 }
 
 export default Page
---
+++ postcss.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  plugins: {
+    // tailwindcss: {}
+    // autoprefixer: {}, // Add other plugins as needed
+  },
+};
