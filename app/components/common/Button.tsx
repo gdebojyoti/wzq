d781f28ef05e33a619046cc365e73368073101ad
Debojyoti Ghosh <gdebojyoti@users.noreply.github.com>
2025-02-24 17:54:45 +0530

    Cursor fix for buttons.

--- app/components/common/Button.tsx
+++ app/components/common/Button.tsx
@@ -1,5 +1,5 @@
 const Button = ({ children, type, onClick, className: overrideClassName = '' }) => {
-  let className = 'w-40 h-10 rounded-full ' + overrideClassName
+  let className = 'w-40 h-10 rounded-full cursor-pointer ' + overrideClassName
 
   switch (type) {
     case 'primary': {
